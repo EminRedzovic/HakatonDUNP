13e85ac9f8af357fc8ab72bfa26aa21747fc18a9
EminRedzovic <redzovicemin200@gmail.com>
2024-12-07 23:18:24 +0100

    create homework

--- src/pages/createHomeWork/createHomeWork.jsx
+++ src/pages/createHomeWork/createHomeWork.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
@@ -14,44 +14,13 @@ const CreateHomeWork = () => {
   const [imagePreview, setImagePreview] = useState(null);
   const [imageFile, setImageFile] = useState(null);
   const homeworkCollection = collection(db, "homework");
-  const token = localStorage.getItem("token");
 
-
-  // Handle image file input change
-  const handleImageChange = (e) => {
-    const file = e.target.files[0]; // Get the file from input
-    setImageInput(file); // Store the file for upload
-    setImage(URL.createObjectURL(file)); // Set preview image
-  };
-
-  useEffect(() => {
-    const importImg = async () => {
-      if (imageInput) {
-        setUploading(true); // Postavite na true dok se slika upload-uje
-        try {
-          const storageRef = ref(storage, `${token + imageInput.name}`);
-          const uploadSnapshot = await uploadBytes(storageRef, imageInput);
-          const downloadUrl = await getDownloadURL(uploadSnapshot.ref);
-          setImageUrls([downloadUrl]);
-          setUploading(false); // Postavite na false kada upload završi
-        } catch (error) {
-          console.error("Greška prilikom upload-a slike: ", error);
-          setUploading(false); // Postavite na false u slučaju greške
-        }
-      }
-    };
-
-    if (imageInput) {
-      importImg();
-    }
-  }, [imageInput, token]);
-
-  // Form validation schema using Yup
   const formik = useFormik({
     initialValues: {
       title: "",
       description: "",
       dueDate: "",
+      file: null,
       image: null,
     },
 
@@ -71,7 +40,8 @@ const CreateHomeWork = () => {
           image: imageFile ? URL.createObjectURL(imageFile) : null,
         };
 
-        await addDoc(homeworkCollection, data); // Add document to Firestore
+        await addDoc(homeworkCollection, data);
+
         toast.success("Uspesno!", {
           position: "top-center",
           autoClose: 5000,
@@ -164,6 +134,23 @@ const CreateHomeWork = () => {
             </div>
 
             <div className="create-homework-input-group">
+              <label htmlFor="file">Priloži PDF Domaćeg</label>
+              <input
+                type="file"
+                id="file"
+                name="file"
+                accept="application/pdf"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.file && formik.errors.file && (
+                <div className="create-homework-error">
+                  {formik.errors.file}
+                </div>
+              )}
+            </div>
+
+            <div className="create-homework-input-group">
               <label htmlFor="image">Dodaj Sliku</label>
               <input
                 type="file"
@@ -182,11 +169,7 @@ const CreateHomeWork = () => {
             </div>
 
             <div className="create-homework-button-div">
-              <button
-                type="submit"
-                className="create-homework-button"
-                disabled={uploading}
-              >
+              <button type="submit" className="create-homework-button">
                 Submit
               </button>
             </div>
