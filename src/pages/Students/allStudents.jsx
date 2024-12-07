89816cfea2e20e4cb6538ee4c50a78eedc8243e8
DarisMavricc <mvrcdaris@gmail.com>
2024-12-07 22:03:50 +0100

    Finished All Users Page

--- src/pages/Students/allStudents.jsx
+++ src/pages/Students/allStudents.jsx
@@ -4,7 +4,7 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 import "./allStudents.css";
 
 const Students = () => {
-  const [selectedRating, setSelectedRating] = useState(3); // Initial rating (e.g., fetched from a database)
+  const [selectedRating, setSelectedRating] = useState(0); // Initial rating (e.g., fetched from a database)
 
   // Function to handle the rating change
   const handleRatingChange = (rating) => {
@@ -21,46 +21,16 @@ const Students = () => {
         <h1 className="text">Svi ucenici</h1>
         <div className="all-students">
           <div className="student">
-            <h1>Ime Ucenika</h1>
-            <h2>Recenzija: 4.7</h2>
-            <StarRating totalStars={5} />
-            <button className="activity-button">
-              Dodaj Vannastavnu aktivnost
-            </button>
-          </div>
-          <div className="student">
-            <h1>Ime Ucenika</h1>
-            <h2>Recenzija: 4.7</h2>
-            <StarRating 
-        initialRating={selectedRating} 
-        totalStars={5} 
-        isEditable={true} // Set to true for editable stars
-        onRatingChange={handleRatingChange} // Pass the function to log rating
-      />
-            <button className="activity-button">
-              Dodaj Vannastavnu aktivnost
-            </button>
-          </div>
-          <div className="student">
-            <h1>Ime Ucenika</h1>
-            <h2>Recenzija: 4.7</h2>
-            <StarRating totalStars={5} />
-            <button className="activity-button">
-              Dodaj Vannastavnu aktivnost
-            </button>
-          </div>
-          <div className="student">
-            <h1>Ime Ucenika</h1>
-            <h2>Recenzija: 4.7</h2>
-            <StarRating totalStars={5} />
-            <button className="activity-button">
-              Dodaj Vannastavnu aktivnost
-            </button>
-          </div>
-          <div className="student">
-            <h1>Ime Ucenika</h1>
-            <h2>Recenzija: 4.7</h2>
-            <StarRating totalStars={5} />
+            <div className="name-and-recension">
+              <h1>Daris Mavric</h1>
+              <h2>Recenzija: 4.7</h2>
+              <StarRating  
+              initialRating={selectedRating} 
+              totalStars={5} 
+              isEditable={true} // Set to true for editable stars
+              onRatingChange={handleRatingChange}
+              />
+            </div>
             <button className="activity-button">
               Dodaj Vannastavnu aktivnost
             </button>
