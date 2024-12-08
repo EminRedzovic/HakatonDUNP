867e3ed323100e5079fb5f192e5746f094bc8744
DanilloPetrovic <danillo.messi@gmail.com>
2024-12-08 03:33:10 +0100

    home v0.1

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,77 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { MdMapsHomeWork } from "react-icons/md";
 import { GiArmorUpgrade } from "react-icons/gi";
 import { FaComment } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import { db } from "../../firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 const Home = () => {
   const [selectedButton, setSelectedButton] = useState(null);
   const navigate = useNavigate();
+  const homeworkCollection = collection(db, "homework");
+  const userCollection = collection(db, "users");
+  const [homeworks, setHomeworks] = useState([]);
+  const [myProfile, setMyProfile] = useState(null);
+  const token = localStorage.getItem("token");
+
+  const getMyProfile = async () => {
+    try {
+      const data = await getDocs(userCollection);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+
+      const userProfile = filteredData.find((user) => user.email === token);
+
+      if (userProfile) {
+        setMyProfile(userProfile);
+      } else {
+        console.error("Korisnik nije pronađen.");
+        setMyProfile(null); // Osiguranje
+      }
+    } catch (error) {
+      console.error("Greška prilikom učitavanja profila:", error);
+    }
+  };
+
+  const getAllHomeWorks = async () => {
+    if (myProfile) {
+      const data = await getDocs(homeworkCollection);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(), // Corrected to access doc.data() for homework data
+        id: doc.id, // Access doc.id directly
+      }));
+
+      const myClassHomeworks = filteredData.filter(
+        (homework) => homework.odeljenje === myProfile.odeljenje
+      );
+
+      setHomeworks(myClassHomeworks);
+    }
+  };
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    const fetchProfile = async () => {
+      try {
+        await getMyProfile();
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchProfile();
+  }, [token]);
+
+  useEffect(() => {
+    getAllHomeWorks();
+  }, [myProfile]);
 
   const handleButtonClick = (button) => {
     setSelectedButton(button);
@@ -21,163 +84,42 @@ const Home = () => {
       </div>
 
       <div className="home-main">
+        <h1 className="home-h1">Domaci zadaci</h1>
+
         <div className="all-homeworks">
-          <div className="completed-work">
-            <div className="complete">
-              <p>Zavrseni Zadaci</p>
-            </div>
-            <div
-              className="work"
-              onClick={() => {
-                navigate("/submitedForm");
-              }}
-            >
-              <div className="work-name">
-                <MdMapsHomeWork size={40} color="white" />
-                <h1>Matematika - sabiranje</h1>
-              </div>
-              <div className="work-name">
-                <GiArmorUpgrade size={40} color="#62b6a9" />
-                <h1
-                  style={{
-                    color: "#62b6a9",
-                  }}
-                >
-                  5 stars
-                </h1>
-              </div>
-              <div className="work-name">
-                <FaComment size={40} color="#D9CD65" />
-                <p>
-                  Moras vise poraditi na obradi brojeva o toku fotosinteze real
-                  madrida kad mbappe
-                </p>
-              </div>
-              <p
-                style={{
-                  textAlign: "center",
-                }}
-              >
-                Klikni na karticu za vise informacija
-              </p>
-            </div>
-            <div
-              className="work"
-              onClick={() => {
-                navigate("/submitedForm");
-              }}
-            >
-              <div className="work-name">
-                <MdMapsHomeWork size={40} color="white" />
-                <h1>Matematika - sabiranje</h1>
-              </div>
-              <div className="work-name">
-                <GiArmorUpgrade size={40} color="#62b6a9" />
-                <h1
-                  style={{
-                    color: "#62b6a9",
-                  }}
-                >
-                  5 stars
-                </h1>
-              </div>
-              <div className="work-name">
-                <FaComment size={40} color="#D9CD65" />
-                <p>
-                  Moras vise poraditi na obradi brojeva o toku fotosinteze real
-                  madrida kad mbappe
-                </p>
-              </div>
-              <p
-                style={{
-                  textAlign: "center",
-                }}
-              >
-                Klikni na karticu za vise informacija
-              </p>
-            </div>
-            <div
-              className="work"
-              onClick={() => {
-                navigate("/submitedForm");
-              }}
-            >
-              <div className="work-name">
-                <MdMapsHomeWork size={40} color="white" />
-                <h1>Matematika - sabiranje</h1>
-              </div>
-              <div className="work-name">
-                <GiArmorUpgrade size={40} color="#62b6a9" />
-                <h1
-                  style={{
-                    color: "#62b6a9",
-                  }}
-                >
-                  5 stars
-                </h1>
-              </div>
-              <div className="work-name">
-                <FaComment size={40} color="#D9CD65" />
-                <p>
-                  Moras vise poraditi na obradi brojeva o toku fotosinteze real
-                  madrida kad mbappe
-                </p>
-              </div>
-              <p
-                style={{
-                  textAlign: "center",
-                }}
-              >
-                Klikni na karticu za vise informacija
-              </p>
-            </div>
+          <div className="nezavrseni-domaci">
+            {homeworks.length > 0 ? (
+              homeworks.map((homework) => (
+                <div className="homework-home">
+                  <div className="homework-home-header">
+                    <h2 className="homework-home-title">{homework.title}</h2>
+                    <p className="homework-home-predmet">{homework.predmet}</p>
+                  </div>
+
+                  <div className="homework-home-down">
+                    <p className="homework-home-description">
+                      {homework.description}
+                    </p>
+                    <p className="homework-home-date">
+                      Rok: {homework.dueDate}
+                    </p>
+
+                    <div className="homework-home-buttons-div">
+                      <button className="homework-home-button">Otvori</button>
+                      <button className="homework-home-button-resi">
+                        Resi
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p className="ndz">Nema domacih zadataka :)</p>
+            )}
           </div>
-          <div className="incomplete-work">
-            <div className="buttons">
-              <div
-                className={`incomplete-check ${
-                  selectedButton === "incomplete" ? "selected" : ""
-                }`}
-                onClick={() => handleButtonClick("incomplete")}
-              >
-                Incomplete
-              </div>
-              <div
-                className={`waiting-check ${
-                  selectedButton === "waiting" ? "selected" : ""
-                }`}
-                onClick={() => handleButtonClick("waiting")}
-              >
-                Waiting for approval
-              </div>
-            </div>
-            <div className="home-works">
-              <div className="work">
-                <h1>Work Name</h1>
-                <h2>Work description</h2>
-                <button className="incomplete-button">Completed</button>
-              </div>
-              <div className="work">
-                <h1>Work Name</h1>
-                <h2>Work description</h2>
-                <button className="incomplete-button">Completed</button>
-              </div>
-              <div className="work">
-                <h1>Work Name</h1>
-                <h2>Work description</h2>
-                <button className="incomplete-button">Completed</button>
-              </div>
-              <div className="work">
-                <h1>Work Name</h1>
-                <h2>Work description</h2>
-                <button className="incomplete-button">Completed</button>
-              </div>
-              <div className="work">
-                <h1>Work Name</h1>
-                <h2>Work description</h2>
-                <button className="incomplete-button">Completed</button>
-              </div>
-            </div>
+
+          <div className="complited-homeworks">
+            <h2 className="zdz">Zavrseni domaci zadaci</h2>
           </div>
         </div>
       </div>
