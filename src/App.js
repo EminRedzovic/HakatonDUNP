3356430bc6f3f63c02a83ff65a35a040ce3bd869
DanilloPetrovic <danillo.messi@gmail.com>
2024-12-08 02:19:25 +0100

    homeworks-pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
-
 import SubmitedForm from "./pages/submitForm/submitedForm";
-
 import TeacherRegister from "./pages/TeacherRegister/TeacherRegister";
 import TeacherLogin from "./pages/TeacherRegister/TeacherLogin";
 import CreateHomeWork from "./pages/createHomeWork/createHomeWork";
 import CreateNewStudent from "./pages/createNewStudent/createNewStudent";
 import Students from "./pages/Students/allStudents";
 import Psychologist from "./pages/psychologist/psychologist";
+import SubjectHomeworks from "./pages/SubjectHomeworks/SubjectHomeworks";
 
 function App() {
   return (
@@ -25,8 +24,8 @@ function App() {
         <Route path="/teacher/students" element={<Students />} />
         <Route path="/register" element={<TeacherRegister />} />
         <Route path="/psychologist" element={<Psychologist />} />
-
         <Route path="/login" element={<TeacherLogin />} />
+        <Route path="/homeworks/:subject" element={<SubjectHomeworks />} />
       </Routes>
     </BrowserRouter>
   );
